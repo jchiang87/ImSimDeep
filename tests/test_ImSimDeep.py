97071b77024c32148f1a3de5f1704130868682c0
Jim Chiang <jchiang@slac.stanford.edu>
2016-11-06 18:18:27 -0800

    account for column name changes in imSim

--- tests/test_ImSimDeep.py
+++ tests/test_ImSimDeep.py
@@ -22,11 +22,11 @@ class ObservedSEDsTestCase(unittest.TestCase):
     def test_get_SED(self):
         "Test the get_SED function."
         # Expected u-band magnitudes:
-        uband_mags = (23.0524608965, 20.030077675)
+        uband_mags = (23.0524608965, 20.0988206)
 
         for i in range(len(self.objects)):
             pars = self.objects.iloc[i]
-            app_mag = desc.imsimdeep.ApparentMagnitude(pars.sedName)
+            app_mag = desc.imsimdeep.ApparentMagnitude(pars.sedFilepath)
             self.assertAlmostEqual(app_mag(pars, 'u'), uband_mags[i])
 
 if __name__ == '__main__':
