948dcb37fcfe7bb2165d53a14ad56f6d96d5ee36
Jim Chiang <jchiang@slac.stanford.edu>
2016-12-06 21:31:31 -0800

    update __init__.py for build_index_files module

--- python/desc/imsimdeep/__init__.py
+++ python/desc/imsimdeep/__init__.py
@@ -4,3 +4,4 @@ from .InstanceCatalogMaker import *
 from .instance_catalog_tools import *
 from .instcat_tools import InstcatTools_sky_cone_select as sky_cone_select
 from .instcat_tools import InstcatTools_ang_sep as ang_sep
+from .build_index_files import *
