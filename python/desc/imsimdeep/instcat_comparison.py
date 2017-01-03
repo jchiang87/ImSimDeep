d74b1da81a63661ea7bf1e6a249d045c98e01b4b
Jim Chiang <jchiang@slac.stanford.edu>
2017-01-02 22:19:33 -0800

    enable comparison with icSrc output, computing the coord_ra, coord_dec using the wcs from calexp

--- python/desc/imsimdeep/instcat_comparison.py
+++ python/desc/imsimdeep/instcat_comparison.py
@@ -15,6 +15,7 @@ import astropy.io.fits as fits
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn.neighbors
+import lsst.afw.image as afwImage
 import lsst.daf.persistence as dp
 import desc.imsim
 
@@ -38,8 +39,84 @@ class MagFromAdu(object):
         "Convert the error in ADU to magnitude error."
         return 2.5*np.log10(self.fluxmag0)*counts_err/counts
 
+class Level2Catalog(object):
+    "Class to encapsulate catalog data."
+    def __init__(self, objectId, coord_ra, coord_dec, flux, fluxerr):
+        self.objectId = np.array(objectId)
+        self.coord_ra = np.array(coord_ra)
+        self.coord_dec = np.array(coord_dec)
+        self.flux = np.array(flux)
+        self.fluxerr = np.array(fluxerr)
+
+    @staticmethod
+    def read_icSrc_file(calexp_file, icSrc_file, flux_col='base_PsfFlux_flux',
+                        pixel_coord_prefix='base_SdssCentroid'):
+        """
+        Read id, coordinate, and flux information from icSrc and calexp
+        files.
+
+        Parameters
+        ----------
+        calexp_file : str
+            FITS file with the calibrated exposure frame.
+        icSrc_file : str
+            FITS file with the catalog information.
+        flux_col : str, optional
+            Flux column to use.  Default: 'base_PsfFlux_flux'
+        pixel_coord_prefix : str, optional
+            Prefix for the pixel coordinate columns to use.
+            Default: 'base_SdssCentroid'
+
+        Returns
+        -------
+        Level2Catalog
+            A Level2Catalog object with objectId, coord_ra, coord_dec, flux,
+            fluxerr info.
+
+        """
+        wcs = afwImage.ExposureF(calexp_file).getWcs()
+        catalog = fits.open(icSrc_file)
+        coord_ra, coord_dec = [], []
+        for x, y in zip(catalog[1].data[pixel_coord_prefix + '_x'],
+                        catalog[1].data[pixel_coord_prefix + '_y']):
+            coords = wcs.pixelToSky(x, y).toIcrs()
+            coord_ra.append(coords.getLongitude().asRadians())
+            coord_dec.append(coords.getLatitude().asRadians())
+        return Level2Catalog(catalog[1].data['id'], coord_ra, coord_dec,
+                             catalog[1].data[flux_col],
+                             catalog[1].data[flux_col + 'Sigma'])
+
+    @staticmethod
+    def read_forced_src_file(forced_src_file, flux_col='base_PsfFlux_flux'):
+        """
+        Read objectId, coordinate, and flux information from forced source
+        files.
+
+        Parameters
+        ----------
+        calexp_file : str
+            FITS file with the calibrated exposure frame.
+        icSrc_file : str
+            FITS file with the catalog information.
+        flux_col : str, optional
+            Flux column to use.  Default: 'base_PsfFlux_flux'
+
+        Returns
+        -------
+        Level2Catalog
+            A Level2Catalog object with objectId, coord_ra, coord_dec, flux,
+            fluxerr info.
+        """
+        forced = fits.open(forced_src_file)
+        return Level2Catalog(forced[1].data['objectId'],
+                             forced[1].data['coord_ra'],
+                             forced[1].data['coord_dec'],
+                             forced[1].data[flux_col],
+                             forced[1].data[flux_col + 'Sigma'])
+
 def instcat_comparison(app_mag_file, repo, visit, raft, sensor,
-                       flux_col='base_PsfFlux_flux', tract='0'):
+                       catalog_type='forced', flux_col='base_PsfFlux_flux',
+                       tract='0'):
     """
     Do a positional association between the forced source objects and
     the instance catalog coordintaes.  Return a data frame with
@@ -60,6 +137,9 @@ def instcat_comparison(app_mag_file, repo, visit, raft, sensor,
         Raft id to use, e.g., '2,2'.
     sensor : str
         Sensor id to use, e.g., '1,1'
+    catalog_type : str, optional
+        Catalog data product to use, either 'forced' or 'icSrc'.
+        Default: 'forced'
     flux_col : str, optional
         Name of the column in the forced source catalog to use for the
         flux measurment.  Default: 'base_PsfFlux_flux'
@@ -76,20 +156,33 @@ def instcat_comparison(app_mag_file, repo, visit, raft, sensor,
         visit number and retrieved band, e.g., 'v230-fr'.
 
     """
-    butler = dp.Butler(repo)
     dataId = dict(visit=visit, raft=raft, sensor=sensor)
-    calexp = butler.get('calexp', dataId=dataId)
+    calexp = dp.Butler(repo).get('calexp', dataId=dataId)
     mag_from_adu = MagFromAdu(calexp.getCalib().getFluxMag0()[0])
     band = calexp.getFilter().getName()
     visit_name = 'v%i-f%s' % (visit, band)
+    raft_name = 'R%s' % raft[::2]
+    sensor_file = 'S%s.fits' % sensor[::2]
 
-    # The data butler doesn't work properly for forced_src catalogs
-    # (It complains with "OperationalError: no such column: tract"
-    # when executing butler.get('forced_src').), so access the data
-    # using astropy.io.fits.
-    forced_catalog = os.path.join(repo, 'forced', tract, visit_name,
-                                  'R%s' % raft[::2], 'S%s.fits' % sensor[::2])
-    forced = fits.open(forced_catalog)
+    if catalog_type == 'forced':
+        # The data butler doesn't work properly for forced_src catalogs
+        # (It complains with "OperationalError: no such column: tract"
+        # when executing butler.get('forced_src').), so access the data
+        # using astropy.io.fits.
+        forced_src_file = os.path.join(repo, 'forced', tract, visit_name,
+                                       raft_name, sensor_file)
+        catalog = Level2Catalog.read_forced_src_file(forced_src_file,
+                                                     flux_col=flux_col)
+    elif catalog_type == 'icSrc':
+        # Easier (and much quicker) to avoid the butler here too.
+        calexp_file = os.path.join(repo, 'calexp', visit_name, raft_name,
+                                   sensor_file)
+        icSrc_file = os.path.join(repo, 'icSrc', visit_name, raft_name,
+                                  sensor_file)
+        catalog = Level2Catalog.read_icSrc_file(calexp_file, icSrc_file,
+                                                flux_col=flux_col)
+    else:
+        raise RuntimeError('Unknown catalog_type: %s', catalog_type)
 
     instcat = pd.read_pickle(app_mag_file)
 
@@ -98,23 +191,22 @@ def instcat_comparison(app_mag_file, repo, visit, raft, sensor,
     tree = sklearn.neighbors.KDTree(
         np.array(((instcat['raICRS'].values*np.pi/180.,
                    instcat['decICRS'].values*np.pi/180.))).transpose())
-    candidates = np.array(((forced[1].data['coord_ra'],
-                            forced[1].data['coord_dec']))).transpose()
+    candidates = np.array(((catalog.coord_ra, catalog.coord_dec))).transpose()
     offset, index = tree.query(candidates, k=1)
 
     # Since the KDTree is Euclidean for 2 dimensions by default,
     # account for the effect of a non-zero latitude and convert from
     # radians to arcsec.
     offset = np.array([x[0] for x in offset])
-    offset *= np.cos(forced[1].data['coord_dec'])*180./np.pi*3600.
+    offset *= np.cos(catalog.coord_dec)*180./np.pi*3600.
 
     # Build the output data frame.
     index = (tuple([x[0] for x in index]),)
-    flux = np.array(forced[1].data[flux_col].tolist())
-    flux_err = np.array(forced[1].data[flux_col + 'Sigma'].tolist())
-    df = pd.DataFrame(dict(objectId=forced[1].data['objectId'].tolist(),
-                           coord_ra=(forced[1].data['coord_ra']*180./np.pi).tolist(),
-                           coord_dec=(forced[1].data['coord_dec']*180./np.pi).tolist(),
+    flux = np.array(catalog.flux.tolist())
+    flux_err = np.array(catalog.fluxerr.tolist())
+    df = pd.DataFrame(dict(objectId=catalog.objectId.tolist(),
+                           coord_ra=(catalog.coord_ra*180./np.pi).tolist(),
+                           coord_dec=(catalog.coord_dec*180./np.pi).tolist(),
                            magnitude=mag_from_adu(flux),
                            magnitude_error=mag_from_adu.error(flux, flux_err),
                            coord_ra_true=instcat['raICRS'].values[index],
@@ -125,7 +217,8 @@ def instcat_comparison(app_mag_file, repo, visit, raft, sensor,
     return df, visit_name
 
 def plot_instcat_comparison(app_mag_file, repo, visit, raft, sensor,
-                            fontsize='x-small', figsize=(8, 8), max_offset=1):
+                            catalog_type='forced', fontsize='x-small',
+                            figsize=(8, 8), max_offset=1):
     """
     Make plots of measured and true positions, fluxes, and measured
     position offsets.
@@ -144,6 +237,9 @@ def plot_instcat_comparison(app_mag_file, repo, visit, raft, sensor,
         Raft id to use, e.g., '2,2'.
     sensor : str
         Sensor id to use, e.g., '1,1'
+    catalog_type : str, optional
+        Catalog data product to use, either 'forced' or 'icSrc'.
+        Default: 'forced'
     fontsize : str or int, optional
         Font size to use in plots.  Default: 'x-small'
     figsize : (float, float), optional
@@ -161,7 +257,8 @@ def plot_instcat_comparison(app_mag_file, repo, visit, raft, sensor,
     plt.rcParams['ytick.labelsize'] = fontsize
     plt.rcParams['figure.figsize'] = figsize
     ccd = 'R%s S%s' % (raft, sensor)
-    df, visit_name = instcat_comparison(app_mag_file, repo, visit, raft, sensor)
+    df, visit_name = instcat_comparison(app_mag_file, repo, visit, raft, sensor,
+                                        catalog_type=catalog_type)
     pointSource = \
         df.query('galSimType=="pointSource" and offset<%f' % max_offset)
     sersic = df.query('galSimType=="sersic" and offset<%f' % max_offset)
